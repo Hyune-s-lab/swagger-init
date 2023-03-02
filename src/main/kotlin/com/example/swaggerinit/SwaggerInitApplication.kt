26ec3f680fa56e46f55b6266bea0a0d003bbe3ad
Hyune-c <choi8608@gmail.com>
2023-03-02 18:12:28 +0900

    init

---
+++ src/main/kotlin/com/example/swaggerinit/SwaggerInitApplication.kt
@@ -0,0 +1,11 @@
+package com.example.swaggerinit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwaggerInitApplication
+
+fun main(args: Array<String>) {
+    runApplication<SwaggerInitApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/swaggerinit/SwaggerInitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.swaggerinit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwaggerInitApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
